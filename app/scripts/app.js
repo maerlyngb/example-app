4769d81e71bdd9f13eeda23619d77be3096521fe
Maerlyn Broadbent <maerlyn.broadbent@ge.com>
2017-06-01 13:26:47 +1000

    add app controller and moved base data to it

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,15 +5,87 @@
     // Register `myApp` component, along with its associated controller and template
     angular.
     module('myApp').
-    component('myApp', {
-        controller: ['$rootScope',
-            function MyAppController($rootScope) {
+    controller('MainCtrl', ['$scope', '$rootScope',
+        function ($scope, $rootScope) {
 
-                $rootScope.name = 'GTS Dashboard';
+            //Global application object
+            $rootScope.App = {
+                name: 'GTS Dashboard',
+                tabs: [{
+                    title: 'Overview',
+                    icon: 'fa-dashboard',
+                    href: '#',
+                    index: 1,
+                    active: true
+                }, {
+                    title: 'Gas Turbines',
+                    icon: 'fa-circle-o',
+                    href: '#',
+                    index: 2,
+                    active: false,
+                    subtabs: [{
+                        title: 'Gas Turbine 1',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Gas Turbine 2',
+                        href: '#',
+                        active: false
+                    }]
+                }, {
+                    title: 'Steam Turbines',
+                    icon: 'fa-dot-circle-o',
+                    href: '#',
+                    index: 3,
+                    active: false,
+                    subtabs: [{
+                        title: 'Steam Turbine 1',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Steam Turbine 2',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Steam Turbine 3',
+                        href: '#',
+                        active: false
+                    }]
+                }, {
+                    title: 'Centrifugal Compressor',
+                    icon: 'fa-circle',
+                    href: '#',
+                    index: 4,
+                    active: false,
+                    subtabs: [{
+                        title: 'Centrifugal Compressor 1',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Centrifugal Compressor 2',
+                        href: '#',
+                        active: false
+                    }]
+                }, {
+                    title: 'Gearbox',
+                    icon: 'fa-cog',
+                    href: '#',
+                    index: 5,
+                    active: false,
+                    subtabs: [{
+                        title: 'Gearbox 1',
+                        href: '#',
+                        active: false
+                    }]
+                }]
+            };
 
-            }
-        ]
-    });
+            $scope.getYear = function(){
+                var date = new Date();
+                return date.getFullYear();
+            };
+        }
+    ]);
 })();
 
 },{}],2:[function(require,module,exports){
@@ -162,77 +234,12 @@
         controller: [
             function SideNavController() {
 
-                this.title = 'GTS Dashboard';
-
-                this.tabs = [{
-                    title: 'Overview',
-                    icon: 'fa-dashboard',
-                    href: '#',
-                    index: 1,
-                    active: true
-                }, {
-                    title: 'Gas Turbines',
-                    icon: 'fa-circle-o',
-                    href: '#',
-                    index: 2,
-                    active: false,
-                    subtabs: [{
-                        title: 'Gas Turbine 1',
-                        href: '#',
-                        active: false
-                    }, {
-                        title: 'Gas Turbine 2',
-                        href: '#',
-                        active: false
-                    }]
-                }, {
-                    title: 'Steam Turbines',
-                    icon: 'fa-dot-circle-o',
-                    href: '#',
-                    index: 3,
-                    active: false,
-                    subtabs: [{
-                        title: 'Steam Turbine 1',
-                        href: '#',
-                        active: false
-                    }, {
-                        title: 'Steam Turbine 2',
-                        href: '#',
-                        active: false
-                    }, {
-                        title: 'Steam Turbine 3',
-                        href: '#',
-                        active: false
-                    }]
-                }, {
-                    title: 'Centrifugal Compressor',
-                    icon: 'fa-circle',
-                    href: '#',
-                    index: 4,
-                    active: false,
-                    subtabs: [{
-                        title: 'Centrifugal Compressor 1',
-                        href: '#',
-                        active: false
-                    }, {
-                        title: 'Centrifugal Compressor 2',
-                        href: '#',
-                        active: false
-                    }]
-                }, {
-                    title: 'Gearbox',
-                    icon: 'fa-cog',
-                    href: '#',
-                    index: 5,
-                    active: false,
-                    subtabs: [{
-                        title: 'Gearbox 1',
-                        href: '#',
-                        active: false
-                    }]
-                }];
             }
-        ]
+        ],
+        bindings: {
+            title: '@', // string
+            tabs: '=' // two-way
+        }
     });
 })();
 
--- app/scripts/components/app/app.component.js
+++ app/scripts/components/app/app.component.js
@@ -4,13 +4,85 @@
     // Register `myApp` component, along with its associated controller and template
     angular.
     module('myApp').
-    component('myApp', {
-        controller: ['$rootScope',
-            function MyAppController($rootScope) {
+    controller('MainCtrl', ['$scope', '$rootScope',
+        function ($scope, $rootScope) {
 
-                $rootScope.name = 'GTS Dashboard';
+            //Global application object
+            $rootScope.App = {
+                name: 'GTS Dashboard',
+                tabs: [{
+                    title: 'Overview',
+                    icon: 'fa-dashboard',
+                    href: '#',
+                    index: 1,
+                    active: true
+                }, {
+                    title: 'Gas Turbines',
+                    icon: 'fa-circle-o',
+                    href: '#',
+                    index: 2,
+                    active: false,
+                    subtabs: [{
+                        title: 'Gas Turbine 1',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Gas Turbine 2',
+                        href: '#',
+                        active: false
+                    }]
+                }, {
+                    title: 'Steam Turbines',
+                    icon: 'fa-dot-circle-o',
+                    href: '#',
+                    index: 3,
+                    active: false,
+                    subtabs: [{
+                        title: 'Steam Turbine 1',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Steam Turbine 2',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Steam Turbine 3',
+                        href: '#',
+                        active: false
+                    }]
+                }, {
+                    title: 'Centrifugal Compressor',
+                    icon: 'fa-circle',
+                    href: '#',
+                    index: 4,
+                    active: false,
+                    subtabs: [{
+                        title: 'Centrifugal Compressor 1',
+                        href: '#',
+                        active: false
+                    }, {
+                        title: 'Centrifugal Compressor 2',
+                        href: '#',
+                        active: false
+                    }]
+                }, {
+                    title: 'Gearbox',
+                    icon: 'fa-cog',
+                    href: '#',
+                    index: 5,
+                    active: false,
+                    subtabs: [{
+                        title: 'Gearbox 1',
+                        href: '#',
+                        active: false
+                    }]
+                }]
+            };
 
-            }
-        ]
-    });
+            $scope.getYear = function(){
+                var date = new Date();
+                return date.getFullYear();
+            };
+        }
+    ]);
 })();
